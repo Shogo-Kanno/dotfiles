16a4cb039dba53910cff39baf2ff9fd717d6cb72
shogo kanno <north2035@yahoo.co.jp>
2024-03-08 17:01:05 +0900

    fifth

--- brew.sh
+++ brew.sh
@@ -7,28 +7,4 @@ if [ "$(uname)" != "Darwin" ] ; then
 	exit 1
 fi
 
-function open_app_store {
-    sleep 1
-    echo "Open the App Store."
-    open -a "App Store"
-}
-
-function login_check {
-  open_app_store
-  while true; do
-    echo -n "$* [Y/n]: (default: n) "
-    read ANS
-    case $ANS in
-      [Yy]*)
-        return 0
-        ;;
-      *)
-        open_app_store
-        ;;
-    esac
-  done
-}
-
-if login_check "Did you login?"; then
   brew bundle --global
-fi
